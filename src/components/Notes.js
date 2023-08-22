9d9f4a413b5fa7e779537ade99f68e98f25974c9
salmanahmad2888 <salman.ahmed2888@gmail.com>
2023-08-22 21:24:32 +0500

    Edit Note Modal

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -8,14 +8,15 @@ const Notes = () => {
     const {notes, getNotes} = context;
     const ref = useRef(null)
 
-    const [note, setNote] = useState({title:"", description:"", tag:"default"})
+    const [note, setNote] = useState({etitle:"", edescription:"", etag:""})
 
     useEffect(() => {
         getNotes();
         // eslint-disable-next-line
     }, [])
-    const updateNote = (note) => {
+    const updateNote = (currentNote) => {
         ref.current.click();
+        setNote({etitle:currentNote.title, edescription:currentNote.description, etag:currentNote.tag});
     }
 
     const handleClick = (e) => {
@@ -34,7 +35,7 @@ const Notes = () => {
         Launch demo modal
         </button>
 
-        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"  aria-hidden="true">
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"  aria-hidden="true">
         <div className="modal-dialog">
             <div className="modal-content">
             <div className="modal-header">
@@ -44,15 +45,15 @@ const Notes = () => {
                 <form className="my-3">
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" id="etitle" name="etitle" aria-describedby="emailHelp" onChange={onChange}/>
+                        <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange}/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Description</label>
-                        <input type="text" className="form-control" id="edescription" name="edescription" onChange={onChange}/>
+                        <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange}/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="tag" className="form-label">Tag</label>
-                        <input type="text" className="form-control" id="etag" name="etag" onChange={onChange}/>
+                        <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange}/>
                     </div>
                     <button type="submit" className="btn btn-primary" onClick={handleClick}>
                         Add Note
